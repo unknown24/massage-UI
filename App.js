49ed13ef1dc23df02d969d9b4407fc5f8fccfaa5
Aep Saepudin <aep@qwords.co.id>
2019-10-17 23:22:51 +0700

    init commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+import FullScreen from './screens/FullScreen'
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  FullScreen
+}, {
+  initialRouteName: 'FullScreen'
+})
+
+export default createAppContainer(AppNavigator);
